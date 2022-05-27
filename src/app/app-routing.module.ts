b877db17e5466ff414b646571f71e37e37452516
Lorenzo Bertacchi <lorenzo.bertacchi@kettydo.com>
2022-05-27 16:56:42 +0200

    Start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BookDetailComponent } from './book-detail/book-detail.component';
-import { BookListComponent } from './book-list/book-list.component';
-import {SplashScreenComponent} from "./splash-screen/splash-screen.component";
+
 
 const routes: Routes = [
-  {
-    path: '',
-    component: SplashScreenComponent
-  },
-  {
-    path: 'books',
-    component: BookListComponent
-  },
-  {
-    path: 'detail/:id',
-    component: BookDetailComponent
-  }
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { SplashScreenComponent } from './splash-screen/splash-screen.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BookListComponent } from './book-list/book-list.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatChipsModule } from '@angular/material/chips';
-import { BookDetailComponent } from './book-detail/book-detail.component';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SplashScreenComponent,
-    BookListComponent,
-    BookDetailComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
